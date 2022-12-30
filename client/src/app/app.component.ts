5a790bdffe31aaa10d48f4cfb15fd24957a86c4a
Raja Kalavala <45046033+RajaKalavala@users.noreply.github.com>
2022-12-30 14:57:23 +0530

    Update Client UI

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+    title = 'client'
+    Chats: string[] = []
+
+    chatForm = new FormGroup({
+        userChat: new FormControl(''),
+    })
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {}
+
+    onSubmit() {
+        let chat = this.chatForm.value.userChat
+        if (chat) {
+            this.Chats.push(chat)
+        }
+
+        console.log('submit clicked - ' + this.chatForm.value.userChat)
+    }
 }
